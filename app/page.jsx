79e79a371d3a2e7fc01c6032ba6db9765ef25de4
ruiyuuu <106572236+ruiyuuu@users.noreply.github.com>
2024-12-20 10:52:48 +0800

    Add files via upload

---
+++ app/page.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Wave from "@/public/Vector 64.svg";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col">
+      <section className="relative  overflow-hidden flex items-center justify-center aspect-[16/6.5] h-[320px] sm:h-auto">
+        <video
+          width="522"
+          height="295"
+          autoPlay
+          loop
+          playsInline
+          muted
+          className="sm:w-full w-fit sm:max-w-full max-w-fit  h-[450px] sm:h-auto"
+          src="/公勝球賽前導片_FA.mp4"
+        ></video>
+        <Image
+          src={Wave}
+          alt="kv"
+          priority
+          className="w-full h-fit absolute sm:bottom-[-8px] bottom-[-2px] left-0"
+          width={1440}
+          height={115}
+        />
+      </section>
+    </main>
+  );
+}
